d141a9fe31eab3ad4328964c2e1e078da141d788
Qiratsumra <saeedqirat43@gmail.com>
2024-12-04 11:35:04 +0500

    first commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
-'use client'
 
+import { Menu } from "lucide-react";
 import Image from "next/image"
 import Link from "next/link"
-import { useState } from "react"
+import {  Sheet, SheetContent, SheetDescription,SheetHeader,SheetTitle,SheetTrigger,} from "@/components/ui/sheet";
 
 
 import { IoIosMenu,IoMdClose } from "react-icons/io";
@@ -27,47 +27,62 @@ const LinksData:Data[] =[
 
 
 export default function Header() {
-  const [open, setOpen] = useState(false)
+  
   return (
-<header className='flex border-b py-3 px-4 sm:px-10 bg-white  font-[sans-serif] min-h-[70px] tracking-wide relative z-50'>
+  <>
 
-{/* Logo Section */}
-      <div className='flex justify-between items-center gap-10 w-full'>
-        <div className="flex justify-center items-start">
-        <Link href="/"><Image src={profile} alt="logo" className='rounded-full w-20 ring-2 ring-pink-500' />
-        </Link>
-        <h1 className="mt-5 mx-5 text-2xl font-bold hover:text-rose-900 text-pink-700">Qirat Saeed</h1>
-        </div>
+
+   <header className="flex border-b py-3 px-4 sm:px-10 bg-white  font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
+   <div className='flex justify-between items-center gap-10 w-full'>
+            {/* logo */}
+            <div className="flex justify-center">
+            <Link href="/"><Image src={profile} alt="logo" className='rounded-full w-20 ring-2 ring-pink-500' />
+            </Link>
+            <h1 className="mt-5 mx-5 text-2xl font-bold hover:text-rose-900 text-pink-700">Qirat Saeed</h1>
+            </div>
+
 
 
 {/* Search Bar */}
-        <div className="bg-white flex px-1 py-1 rounded-full max-lg:hidden border border-pink-700 overflow-hidden max-w-md mx-auto font-[sans-serif]">
+<div className="bg-white flex px-1 py-1 rounded-full max-lg:hidden border border-pink-700 overflow-hidden max-w-md mx-auto font-[sans-serif]">
         <input type='email' placeholder='Search Something...' className="w-full outline-none bg-white pl-4 text-sm" />
         <button type='button'
           className="bg-pink-500 hover:bg-pink-800 transition-all text-white text-sm rounded-full px-5 py-2.5">Search</button>
       </div>
 
-{/* Toggle Button Section */}
-        <div className={`flex justify-between items-center md:block ${
-              open? "block" : "hidden"}`}>
 
-          <ul
-            className='sm:flex ms:ml-14 sm:gap-x-5 max-sm:space-y-5 max`-sm:pt-28 max-sm:text-pink-700  max-sm:backdrop-blur-sm max-sm:w-1/2 max-sm:min-w-[300px] max-sm:p-6 max-sm:h-full max-lg:shadow-md max-lg:overflow-auto z-50m '>
-           {LinksData.map((item, index)=>(
-            <Link key={index} href={item.path}>
-                 <li className='mb-6 font-serif font-bold text-pink-700 hover:text-pink-600 max-lg:block'>
-             {item.name}
-           </li>
-            </Link>
-           ))}
-           
-          </ul>
-        </div>
-
-        <button id="toggleOpen" className='lg:hidden ml-auto' onClick={()=>setOpen(!open)}>
-        {open ? (<IoMdClose className="bg-white text-pink-700 cursor-pointer" size={24}/>):(<IoIosMenu className="bg-white cursor-pointer text-pink-700" size={24}/>)}
-        </button>
-      </div>
-    </header>
+            {/* nav links */}
+            <div  className='flex justify-between items-center md:block'>
+            <ul className="hidden md:block lg:flex lg:justify-evenly gap-16  font-serif font-bold text-pink-700 hover:text-pink-600 ">
+            {LinksData.map((list)=>(
+                  <li className="space-x-5">
+                  <Link href={`${list.path}`}>{list.name}</Link>
+                </li>
+                ))             
+}
+            </ul>
+                
+           </div>
+            
+            <Sheet >
+              <SheetTrigger className="md:hidden">
+              <Menu className="text-2xl"/>
+              </SheetTrigger>
+              <SheetContent >
+                <SheetHeader>
+                  <SheetTitle className="mt-5 mx-5 text-2xl font-bold hover:text-rose-900 text-pink-700">Menu Bar</SheetTitle>
+                  <ul>
+                {LinksData.map((list)=>(
+                  <li className="flex flex-col space-y-6 mt-5 mx-5 text-2xl font-bold hover:text-rose-900 text-pink-700">
+                  <Link href={`${list.path}`}>{list.name}</Link></li>
+                ))}
+               
+            </ul>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+</div>
+        </header>   
+</>
   )
 }
